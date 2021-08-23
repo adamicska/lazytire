acbae8f3f5e439a8ade5d91ba31679d8d5af1790
petu <adamicska>
2021-08-23 19:34:27 -0300

    title update

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const navigation = [
 export default function IndexPage() {
   return (
     <Layout>
-      <Seo title="Adamicska | App Developer" />
+      <Seo title="Bricklayer | App Development" />
       <div className="relative bg-white overflow-hidden">
         <div className="max-w-7xl mx-auto">
           <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
