2c00ad59f2e3a54320dc20d62f2e0115604dd5d9
petu <adamicska>
2021-08-24 16:02:10 -0300

    redesign

--- src/components/about.js
+++ src/components/about.js
@@ -38,15 +38,15 @@ export default function About() {
     <div id="about" className="my-6 py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-base text-blue-600 font-semibold tracking-wide uppercase">
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
             Skills
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             What we do
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            We use the latest tech to offer you the best performing, but at the
-            same time, easiest to use software for your company
+            We use the latest tech to offer you the best performing and most
+            user friendly software for your company
           </p>
         </div>
 
@@ -55,7 +55,7 @@ export default function About() {
             {features.map(feature => (
               <div key={feature.name} className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                     <feature.icon className="h-6 w-6" aria-hidden="true" />
                   </div>
                   <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
--- src/pages/index.js
+++ src/pages/index.js
@@ -49,13 +49,13 @@ export default function IndexPage() {
                   <div className="flex items-center justify-between flex-grow flex-shrink-0 lg:flex-grow-0">
                     <div className="flex items-center w-full md:w-auto">
                       <PaperAirplaneIcon
-                        className="h-8 w-8 text-blue-600"
+                        className="h-8 w-8 text-indigo-600"
                         aria-hidden="true"
                       />
-                      <strong className="text-gray-900 ">Logo</strong>
+                      <strong className="text-gray-900 ">Bricklayer</strong>
                     </div>
                     <div className="-mr-2 flex items-center md:hidden">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Open main menu</span>
                         <MenuIcon className="h-6 w-6" aria-hidden="true" />
                       </Popover.Button>
@@ -98,12 +98,12 @@ export default function IndexPage() {
                     <div className="px-5 pt-4 flex items-center justify-between">
                       <div>
                         <PaperAirplaneIcon
-                          className="h-8 w-8 text-blue-600"
+                          className="h-8 w-8 text-indigo-600"
                           aria-hidden="true"
                         />
                       </div>
                       <div className="-mr-2">
-                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                           <span className="sr-only">Close main menu</span>
                           <XIcon className="h-6 w-6" aria-hidden="true" />
                         </Popover.Button>
@@ -120,7 +120,7 @@ export default function IndexPage() {
                         </a>
                       ))}
                     </div>
-                    <a
+                    {/* <a
                       href="https://github.com/adamicska"
                       className="block px-3 py-2 ml-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                     >
@@ -131,7 +131,7 @@ export default function IndexPage() {
                       className="block px-3 py-2 ml-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                     >
                       <FontAwesomeIcon icon={faStackOverflow} size="lg" />
-                    </a>
+                    </a> */}
                     <a
                       href="https://t.me/adamicska"
                       className="block px-3 py-2 ml-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
@@ -149,7 +149,7 @@ export default function IndexPage() {
                 </h1>
                 <div className="flex flex-row">
                   <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                    <span className="block text-blue-600 xl:inline">
+                    <span className="block text-indigo-600 xl:inline">
                       Lazy Tire.
                     </span>
                   </h1>
@@ -163,7 +163,7 @@ export default function IndexPage() {
                   <div className="rounded-md shadow">
                     <a
                       href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-black md:py-4 md:text-lg md:px-10"
                     >
                       Get in touch
                     </a>
@@ -171,7 +171,7 @@ export default function IndexPage() {
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <Link
                       to="#about"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
+                      className="w-full flex items-center justify-center shadow px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                     >
                       Learn more
                     </Link>
@@ -184,7 +184,7 @@ export default function IndexPage() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <StaticImage
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://source.unsplash.com/rMyel7micAg"
+            src="https://source.unsplash.com/QckxruozjRg"
             alt="native app developer"
           />
         </div>
